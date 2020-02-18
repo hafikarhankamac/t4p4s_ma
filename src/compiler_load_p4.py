f4a08c2e9033b35c5e7466c4d472eeab3b29ea18
Dominik Scholz <scholz@net.in.tum.de>
2020-02-18 13:20:38 +0100

    HLIR generation: fail with an actual error...

--- src/compiler_load_p4.py
+++ src/compiler_load_p4.py
@@ -35,6 +35,7 @@ def is_cache_file_loadable(path):
 def import_modules(required_modules):
     for modname in required_modules:
         if not pkgutil.find_loader(modname):
+            print('ERROR: Unable to find module {}'.format(modname), file=sys.stderr)
             return None
 
     return [importlib.import_module(modname) for modname in required_modules]

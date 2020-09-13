b25626cb45c531d9344aabe2b3e36b2184ff5874
Mario Silaci <mario.silaci@tum.de>
2020-09-13 19:24:21 +0200

    Correction of hashing & adding debug information

--- src/hardware_dep/dpdk/includes/dpdk_hash.h
+++ src/hardware_dep/dpdk/includes/dpdk_hash.h
@@ -1,58 +1,46 @@
 #include <stdio.h>
 #include "util_packet.h"
 #include <zlib.h>
+#include <inttypes.h>
 
-
-uint8_t * hash_identity(struct uint8_buffer_s data, int size){
+void hash_identity(uint8_t* hash_value, struct uint8_buffer_s data, int size){
 	debug("   :: Hashed with Identity\n");
-	static uint8_t result[sizeof(size)];
 	for(int i = 0; i < size; i++) {
-		result[i] = data.buffer[i];
+		hash_value[size-1-i] = data.buffer[i%data.buffer_size];
 	}
-	return &result;
 }
 
-uint8_t * hash_crc32(struct uint8_buffer_s data, int size){
+void hash_crc32(uint8_t* hash_value, struct uint8_buffer_s data, int size){
 	debug("   :: Hashed with CRC32\n");
-	static uint8_t result[sizeof(size)];
 	uLong crc = crc32(0L, Z_NULL, 0);
-	for (int i = 0; i < data.buffer_size; i++) {
-		crc = crc32(crc, &data.buffer[i], 1);
-	}
-	for (int i = 0; i<size; i++) {
-		result[i] = (crc>>(i%sizeof(crc))*8)&0xFF;
-	}
-	return &result;
+	crc = crc32(crc, data.buffer, data.buffer_size);
+	if (sizeof(crc) < size) {size = sizeof(crc);}
+
+	memcpy(hash_value, &crc, size);
 }
 
-uint8_t * hash_crc32_custom(struct uint8_buffer_s data, int size){
+void hash_crc32_custom(uint8_t* hash_value, struct uint8_buffer_s data, int size){
 	debug("   :: Simulated hashing of CRC32-Custom\n");
-	return 0;
 }
 
-uint8_t * hash_crc16(struct uint8_buffer_s data, int size){
+void hash_crc16(uint8_t* hash_value, struct uint8_buffer_s data, int size){
 	debug("   :: Simulated hashing of CRC16\n");
-	return 0;
 }
 
-uint8_t * hash_crc16_custom(struct uint8_buffer_s data, int size){
+void hash_crc16_custom(uint8_t* hash_value, struct uint8_buffer_s data, int size){
 	debug("   :: Simulated hashing of CRC16-Custom\n");
-	return 0;
 }
 
-uint8_t * hash_csum16(struct uint8_buffer_s data, int size){
+void hash_csum16(uint8_t* hash_value, struct uint8_buffer_s data, int size){
 	debug("   :: Simulated hashing of CSUM16\n");
-	return 0;
 }
 
-uint8_t * hash_random(struct uint8_buffer_s data, int size){
+void hash_random(uint8_t* hash_value, struct uint8_buffer_s data, int size){
 	debug("   :: Simulated hashing of RANDOM\n");
-	return 0;
 }
 
-uint8_t * hash_xor16(struct uint8_buffer_s data, int size){
+void hash_xor16(uint8_t* hash_value, struct uint8_buffer_s data, int size){
 	debug("   :: Simulated hashing of COR16\n");
-	return 0;
 }
 
 
@@ -61,48 +49,80 @@ void hash(uint8_t* hash_start, int hash_length, enum enum_HashAlgorithm algorith
 	if (max > 0) {
 		switch(algorithm) {
 			case enum_HashAlgorithm_crc32:
-//				hash_value = hash_crc32(data, hash_length);
-				memcpy(hash_start, hash_crc32(data, hash_length), hash_length);
+				hash_crc32(hash_start, data, hash_length);
 				break;
 			case enum_HashAlgorithm_crc32_custom:
-				//memcpy(hash_result, &(hash_crc32_custom(data, size), size);
-				//sheep((uint32_t) 100, pd, tables);
+				hash_crc32_custom(hash_start, data, hash_length);
+				sheep((uint32_t) 100, pd, tables);
 				break;
 			case enum_HashAlgorithm_crc16:
-				//memcpy(hash_result, &(hash_crc16(data, size), size);
-				//sheep((uint32_t) 1000, pd, tables);
+				hash_crc16(hash_start, data, hash_length);
+				sheep((uint32_t) 1000, pd, tables);
 				break;
 			case enum_HashAlgorithm_crc16_custom:	
-				//memcpy(hash_result, &(hash_crc16_custom(data, size), size);
-				//sheep((uint32_t) 10000, pd, tables);
+				hash_crc16_custom(hash_start, data, hash_length);
+				sheep((uint32_t) 10000, pd, tables);
 				break;
 			case enum_HashAlgorithm_random:
-				//memcpy(hash_result, &(hash_random(data, size), size);
-				//sheep((uint32_t) 100, pd, tables);
+				hash_random(hash_start, data, hash_length);
+				sheep((uint32_t) 100, pd, tables);
 				break;
 			case enum_HashAlgorithm_identity:	
-//				hash_value = hash_identity(data, hash_length);
-				memcpy(hash_start, hash_identity(data, hash_length), hash_length);
+				hash_identity(hash_start, data, hash_length);
 				break;
 			case enum_HashAlgorithm_csum16:	
-				//memcpy(hash_result, &(hash_csum16(data, size), size);
-				//sheep((uint32_t) 100, pd, tables);
+				hash_csum16(hash_start, data, hash_length);
+				sheep((uint32_t) 100, pd, tables);
 				break;
 			case enum_HashAlgorithm_xor16:
-				//memcpy(hash_result, &(hash_xor16(data, size), size);
-				//sheep((uint32_t) 100, pd, tables);
+				hash_xor16(hash_start, data, hash_length);
+				sheep((uint32_t) 100, pd, tables);
 				break;
 			default:
-//				for (int i = 0; i< hash_length; i++) {
-//					hash_value[i] = 0;
-//				}
+				for (int i = 0; i<hash_length; i++) {
+					hash_value[i] = 0xFF;
+				}
 				debug("   :: Invalid hash method chosen");
 				break;
 		}
 	}else{
-//		for (int i = 0; i< hash_length; i++) {
-//			hash_value[i] = 0;
-//		}
+		//TODO: Base
+		for (int i = 0; i< hash_length; i++) {
+			hash_value[i] = 0;
+		}
 	}
+#ifdef T4P4S_DEBUG
+	switch(hash_length) {
+		case 1:
+			debug("    : Hashed to " T4LIT(%x) "\n", *hash_start);
+			break;
+		case 2:
+			debug("    : Hashed to " T4LIT(%x) "\n", *(uint16_t*)hash_start);
+			break;
+		case 3:
+		case 4:
+			debug("    : Hashed to " T4LIT(%x) "\n", *(uint32_t*)hash_start);
+			break;
+		case 5:
+			
+		case 6:
+		case 7:
+		case 8:
+			debug("    : Hashed to %" PRIX64 "\n", *(uint64_t*)hash_start);
+			break;
+		case 9:
+		case 10:
+		case 11:
+		case 12:
+		case 13:
+		case 14:
+		case 15:
+		case 16:
+			debug("    : Hashed to %" PRIX64 " %" PRIX64 "\n", *(uint64_t*)(hash_start+8), *(uint64_t*)(hash_start));
+			break;
+		default:
+			debug("    : Hashed to value > 128 bit\n");
+	}
+#endif
 
 }

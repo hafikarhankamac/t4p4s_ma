6af34b254ddb0fc987e47aeb558dc47cf5a906e7
root <root@tapa.baltikum.net.in.tum.de>
2021-04-23 17:26:02 +0200

    WIP

--- src/hardware_dep/dpdk/data_plane/dpdk_timer.c
+++ src/hardware_dep/dpdk/data_plane/dpdk_timer.c
@@ -27,7 +27,7 @@ void timer_callback_periodic(struct rte_timer *tim, void *arg)
 {
     timer_event_t *timer_event = (timer_event_t*) arg;
     timer_event->repeat++;
-    enque_event(lcore_conf[timer_event->lcore].state.event_queue, TIMER, timer_event->id);
+    fill_event_queue(lcore_conf[timer_event->lcore].state.event_queue, TIMER, timer_event->id, 31);
 }
 
 void timer_callback_multiple(struct rte_timer *tim, void *arg)
@@ -55,6 +55,7 @@ void single_timer_lcore(uint32_t ms, uint32_t id, uint32_t lcore)
 
 void periodic_timer_lcore(uint32_t ms, uint32_t id, uint32_t lcore)
 {
+	if (!already[lcore]) {
     timer_event_t *timer_event = rte_malloc("timer", sizeof(timer_event_t), 0);
     struct rte_timer *rtetimer = rte_malloc("rte_timer", sizeof(struct rte_timer), 0);
     timer_event->repeat = 0;
@@ -62,6 +63,8 @@ void periodic_timer_lcore(uint32_t ms, uint32_t id, uint32_t lcore)
     timer_event->id = id;
     rte_timer_init(rtetimer);
     rte_timer_reset_sync(rtetimer, hz_millis * ms, PERIODICAL, lcore, timer_callback_periodic, (void*) timer_event);
+    already[lcore] = true;
+	}
 }
 
 void multiple_timer_lcore(uint32_t ms, uint32_t id, uint32_t count, uint32_t lcore)
@@ -92,6 +95,7 @@ void multiple_timer(uint32_t ms, uint32_t id, uint32_t count)
 
 void timer_init(uint64_t hz) 
 {
-	hz_millis = hz / 1000;
+	hz_millis = 1;
+	//hz_millis = hz / 1000;
 	memset(already, false, RTE_MAX_LCORE);
 }

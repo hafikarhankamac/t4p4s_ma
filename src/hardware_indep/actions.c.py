6f8c1fa2d6fbba5e42a41fdfe13a518d0c3ba9a2
Manuel Simon <manuel.simon@tum.de>
2021-05-19 13:36:46 +0200

    merge fixes

--- src/hardware_indep/actions.c.py
+++ src/hardware_indep/actions.c.py
@@ -67,26 +67,3 @@ for table in hlir.tables:
 #} };
 #[
 
-for ctl in hlir.controls:
-    for act in ctl.actions:
-        #{ void action_code_${act.name}(action_${act.name}_params_t *parameters, SHORT_STDPARAMS) {
-        #[     uint32_t value32, res32, mask32;
-        #[     (void)value32; (void)res32; (void)mask32;
-        #[     control_locals_${ctl.name}_t* local_vars = (control_locals_${ctl.name}_t*) pd->control_locals;
-
-        for stmt in act.body.components:
-            global pre_statement_buffer
-            global post_statement_buffer
-            pre_statement_buffer = ""
-            post_statement_buffer = ""
-
-            code = format_statement(stmt, ctl)
-            if pre_statement_buffer != "":
-                #= pre_statement_buffer
-                pre_statement_buffer = ""
-            #= code
-            if post_statement_buffer != "":
-                #= post_statement_buffer
-                post_statement_buffer = ""
-        #} }
-        #[

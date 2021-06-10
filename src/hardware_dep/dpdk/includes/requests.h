7bbb3ceb8c6537811b19d8820c19da857160640c
Manuel Simon <simonm@net.in.tum.de>
2021-06-10 11:03:14 +0200

    refactored requests.h

--- src/hardware_dep/dpdk/includes/requests.h
+++ src/hardware_dep/dpdk/includes/requests.h
@@ -1,7 +1,7 @@
 #pragma once
 
-typedef uint64_t digest_t;
-typedef uint64_t cp_digest_t;
+typedef uint32_t digest_t;
+typedef uint32_t cp_digest_t;
 typedef uint16_t cp_id;
 
 typedef struct {
@@ -9,45 +9,36 @@ typedef struct {
 } request_store_t;
 
 
-    request_store_t* request_store(uint64_t size) {
+    request_store_t* request_store(uint32_t size) {
         return (request_store_t*) rte_malloc("request_store_t", size * sizeof(uint8_t), 0)
     }
 
-    bool isDelivered(request_store_t *rs, digest_t digest) {
-        return false; // TODO stub
+    void extern_request_store_isDelivered(uint32_t declarg, bool *del, digest_t digest, request_store_t *rs, SHORT_STDPARAMS_IN)
     }
-    request_payload_t getByDigest(request_store_t *rs, digest_t digest) {
-        return NULL;
+    void extern_request_store_getByDigest(uint32_t declarg, request_payload_t *reqpl, digest_t digest, request_store_t *rs, SHORT_STDPARAMS_IN) {
     }
 
-    cp_digest_t createCheckpoint(request_store_t *rs, uint32_t lv, uint64_t sn, uint16_t ID) {
-        return 0;
+    void extern_request_store_createCheckpoint(uint32_t declarg, cp_digest_t *cp, uint32_t lv, uint32_t sn, uint16_t ID,  request_store_t *rs, SHORT_STDPARAMS_IN) {
     }
 
-    digest_t add(request_store_t *rs, uint16_t ID, uint64_timestamp, request_payload_t request) {
-        return 0;
+    void extern_request_store_add(uint32_t declarg, digest_t *dig, uint16_t ID, uint32_t timestamp, request_payload_t request, request_store_t *rs, SHORT_STDPARAMS_IN) {
     }
 
-    digest_t add_request(request_store_t *rs ,request_t r) {
-        return 0;
+    void extern_request_store_add_request(uint32_t declarg, digest_t *dig, request_t r,  request_store_t *rs, SHORT_STDPARAMS_IN) {
     }
 
-    cp_id updateCheckpoint(request_store_t *rs, uint64_t cp_digest, uint16_t checkpoint_id);
+    void extern_request_store_updateCheckpoint(uint32_t declarg, cp_id * cp, uint32_t cp_digest, uint16_t checkpoint_id,  request_store_t *rs, SHORT_STDPARAMS_IN);
 
     //TODO delivered=true
-    void commit(request_store_t *rs, digest_t digest) {
+    void extern_request_store_commit(uint32_t declarg, request_store_t *rs, digest_t digest) {
     }
 
-    digest_t getDigest(request_store_t *rs, uint8_t req, uint64_t args) {
-        return 0;
+    void extern_request_store_getDigest(uint32_t declarg, digest_t *dig, uint8_t req, uint32_t args, request_store_t *rs, SHORT_STDPARAMS_IN) {
     }
 
-    digest_t getDigest(request_store_t *rs, request_payload_t request) {
-        return 0;
+    void extern_request_store_getDigest(uint32_t declarg, digest_t *dig, request_payload_t request,  request_store_t *rs, SHORT_STDPARAMS_IN) {
     }
 
-
-	bool contains(request_store_t *rs, digest_t digest) {
-	    return false;
+	void extern_request_store_contains(uint32_t declarg, bool *ret, digest_t digest,  request_store_t *rs, SHORT_STDPARAMS_IN) {
 	}
 }

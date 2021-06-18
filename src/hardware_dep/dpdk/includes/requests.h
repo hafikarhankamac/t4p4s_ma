381b3889add6dfe6a5a4876988d8b947682bddda
Manuel Simon <simonm@net.in.tum.de>
2021-06-18 15:58:27 +0200

    refactored requests.h requests.c according to request.p4

--- src/hardware_dep/dpdk/includes/requests.h
+++ src/hardware_dep/dpdk/includes/requests.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <rte_malloc.h>
+#include <rte_hash.h>
+
 #include "backend.h"
 #include "dpdk_tables.h"
 #include "common.h"
@@ -11,6 +13,8 @@ typedef uint16_t cp_id;
 
 
 struct request_store {
+
+    rte_hash *table;
     uint8_t *anon;
 };
 
@@ -23,7 +27,7 @@ void extern_request_store_createCheckpoint(uint32_t declarg, cp_digest_t *cp, ui
 void extern_request_store_add(uint32_t declarg, digest_t *dig, uint16_t ID, uint32_t timestamp, request_payload_t *request, request_store_t *rs, SHORT_STDPARAMS);
     //void extern_request_store_add_request(uint32_t declarg, digest_t *dig, request_t r,  request_store_t *rs, SHORT_STDPARAMS) {
     //}
-void extern_request_store_add_request(uint32_t declarg, uint32_t *dig, uint32_t timestamp, uint16_t cliendId, uint8_t req, uint32_t args, request_store_t *rs, SHORT_STDPARAMS);
+void extern_request_store_add_request(uint32_t declarg, uint32_t *dig, uint32_t sn, uint32_t lv, uint16_t cliendId, uint8_t req, uint32_t args, request_store_t *rs, SHORT_STDPARAMS);
 void extern_request_store_updateCheckpoint(uint32_t declarg, cp_id * cp, uint32_t cp_digest, uint16_t checkpoint_id,  request_store_t *rs, SHORT_STDPARAMS);
 
     //TODO delivered=true
@@ -34,4 +38,5 @@ void extern_request_store_getDigest(uint32_t declarg, digest_t *dig, uint8_t req
 //    }
 
 void extern_request_store_contains(uint32_t declarg, bool *ret, digest_t digest,  request_store_t *rs, SHORT_STDPARAMS);
+void extern_request_store_containsSn(uint32_t declarg, bool *ret, digest_t digest, uint32_t sn, uint32_t lv, request_store_t *rs, SHORT_STDPARAMS);
 

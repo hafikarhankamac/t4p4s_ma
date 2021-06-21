8d4073900186054a42cb0c1ad7810cbb443d099e
Manuel Simon <root@valga.baltikum.net.in.tum.de>
2021-06-21 20:28:57 +0200

    fix compile errors in requests

--- src/hardware_dep/dpdk/data_plane/externs/requests.c
+++ src/hardware_dep/dpdk/data_plane/externs/requests.c
@@ -15,17 +15,31 @@ request_store_t* request_store(uint32_t size, SHORT_STDPARAMS)
 
 
 
-uint32_t hash(uint8_t *key, uint8_t length) {
+uint64_t get_sn_lv_key(uint32_t sn, uint32_t lv) {
+    uint64_t snlv = sn << 32;
+    snlv |= lv;
+    return snlv;
+}
+
+
+uint32_t hash_naive(uint8_t *key, uint8_t length) {
    uint32_t h = 0;
-   for(int i = 0, i < length; i++) {
+   for(int i = 0; i < length; i++) {
        h += key[i];
    }
 
    return h;
 }
 
-inline uint32_t hash_req(request_t *req) {
-    return hash(req, sizeof(request_t));
+void hash_request(request_payload_t *req, uint8_t **hsh) {
+   uint8_t key[5];
+   key[0] = req->req;
+   key[1] = req->args;
+   *hsh = hash_naive(key, 5);
+}
+
+uint32_t hash_req(request_t *req) {
+    return hash_naive(req, sizeof(request_t));
 }
 
 void extern_request_store_isDelivered(uint32_t declarg, bool *del, digest_t digest, request_store_t *rs, SHORT_STDPARAMS)
@@ -40,7 +54,7 @@ void extern_request_store_getByDigest(uint32_t declarg, request_payload_t *reqpl
    request_t *req;
    rte_hash_lookup_with_hash_data(rs->table, digest, digest, &req);
    memcpy(reqpl, req->payload, sizeof(request_payload_t));
-   *reqpl = req->payload;
+   reqpl = &(req->payload);
 }
 
 void extern_request_store_createCheckpoint(uint32_t declarg, cp_digest_t *cp, uint32_t lv, uint32_t sn, uint16_t ID,  request_store_t *rs, SHORT_STDPARAMS)
@@ -54,32 +68,20 @@ void extern_request_store_add(uint32_t declarg, digest_t *dig, uint16_t ID, uint
     //void extern_request_store_add_request(uint32_t declarg, digest_t *dig, request_t r,  request_store_t *rs, SHORT_STDPARAMS) {
     //}
 
-void extern_request_store_add_request(uint32_t declarg, uint32_t *dig, uint32_t sn, uint32_t lv, uint16_t cliendId, uint8_t req, uint32_t args, request_store_t *rs, SHORT_STDPARAMS)
+void extern_request_store_add_request(uint32_t declarg, uint32_t *dig, uint32_t sn, uint32_t lv, uint16_t clientId, uint8_t req_cmd, uint32_t args, request_store_t *rs, SHORT_STDPARAMS)
 {
-   request_t *req = rte_malloc("request_to_store_t", sizeof(request_to_store_t), 0);
-   req->request->payload = { .req = req, .args = args};
-   req->request->clientId = clientId;
-   req->sn = sn;
-   req->lv = lv;
-   hash_request(&req->request, *dig);
-   rte_add_key_with_hash_data(rs->table, dig, dig, req);
-   uint64_t snlv = get_sn_lv_key(sn, lv);
-   rte_add_key_data(rs->snlv, snlv, req);
+    request_to_store_t *req = rte_malloc("request_to_store_t", sizeof(request_to_store_t), 0);
+    req->sn = sn; 
+    req->lv = lv;
+    req->request.clientId = clientId,
+    req->request.payload->req = req_cmd;
+    req->request.payload->args = args;
+    hash_request(&req->request, *dig);
+    rte_hash_add_key_with_hash_data(rs->table, dig, dig, req);
+    uint64_t snlv = get_sn_lv_key(sn, lv);
+    rte_hash_add_key_data(rs->snlv, snlv, req);
 
-   max_not_executed = max_not_executed > sn ? max_not_executed : sn;
-}
-
-inline uint64_t get_sn_lv_key(uint32_t sn, uint32_t lv) {
-    uint64_t snlv = sn << 32;
-    snlv |= lv;
-    return snlv;
-}
-
-inline void hash_request(request_t *req, uint8_t *hash) {
-   uint8_t key[5];
-   key[0] = req->req;
-   key[1] = req->arg;
-   *hash = hash(key, 5);
+    rs->max_not_executed = rs->max_not_executed > sn ? rs->max_not_executed : sn;
 }
 
 void extern_request_store_updateCheckpoint(uint32_t declarg, cp_id * cp, uint32_t cp_digest, uint16_t checkpoint_id,  request_store_t *rs, SHORT_STDPARAMS)
@@ -88,7 +90,7 @@ void extern_request_store_updateCheckpoint(uint32_t declarg, cp_id * cp, uint32_
 }
 
 //TODO delivered=true
-void extern_request_store_commit(uint32_t declarg, digest_t digest, uint32_t last_exec, request_store_t *rs, SHORT_STDPARAMS)
+void extern_request_store_commit(uint32_t declarg, digest_t digest, request_store_t *rs, SHORT_STDPARAMS)
 {
     request_to_store_t *req;
     rte_hash_lookup_with_hash_data(rs->table, digest, digest, &req);
@@ -99,12 +101,12 @@ void extern_request_store_commit(uint32_t declarg, digest_t digest, uint32_t las
 
     request_to_store_t *r;
     for (uint32_t i = rs->min_not_executed; i <= max; i++) {
-        uint64_t snlv = get_sn_lv_key(sn, lv);
+        uint64_t snlv = get_sn_lv_key(i, lv);
         rte_hash_lookup_with_hash_data(rs->snlv, snlv, snlv, &r);
         if (req->request.processed) {
-            min_not_executed = r->sn;
+            rs->min_not_executed = r->sn;
         } else {
-            raise_event(PROCESS_REQUEST, r->request.dig);
+            raise_event(PROCESS_REQUEST, digest);
             r->request.processed = true;
         }
     }
@@ -112,7 +114,7 @@ void extern_request_store_commit(uint32_t declarg, digest_t digest, uint32_t las
 
 void extern_request_store_getDigest(uint32_t declarg, digest_t *dig, uint8_t req, uint32_t args, request_store_t *rs, SHORT_STDPARAMS)
 {
-    struct request = {.req = req, .args = args};
+    request_payload_t request = {.req = req, .args = args};
     hash(&request, *dig);
 }
 

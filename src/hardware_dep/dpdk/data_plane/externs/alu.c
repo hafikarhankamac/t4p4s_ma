f3e7643362ce520af924a5b771acb5085809c86f
root <root@litecoin.blockchain.net.in.tum.de>
2021-08-02 16:22:49 +0200

    fix alu definition

--- src/hardware_dep/dpdk/data_plane/externs/alu.c
+++ src/hardware_dep/dpdk/data_plane/externs/alu.c
@@ -1,16 +1,19 @@
 #include "alu.h"
 
-void modulo_8(uint32_t declargs, uint8_t *ret, uint8_t div, uint8_t mod, alu_t* alu, SHORT_STDPARAMS) {
+alu_t* alu(SHORT_STDPARAMS) {
+}
+
+void extern_alu_modulo_8(uint8_t *ret, uint8_t div, uint8_t mod, alu_t* alu, SHORT_STDPARAMS) {
     *ret = div % mod;
 }
-void modulo_16(uint32_t declargs, uint16_t *ret, uint16_t div, uint16_t mod, alu_t* alu, SHORT_STDPARAMS) {
+void extern_alu_modulo_16(uint16_t *ret, uint16_t div, uint16_t mod, alu_t* alu, SHORT_STDPARAMS) {
     *ret = div % mod;
 }
 
-void modulo_32(uint32_t declargs, uint32_t *ret, uint32_t div, uint32_t mod, alu_t* alu, SHORT_STDPARAMS) {
+void extern_alu_modulo_32(uint32_t *ret, uint32_t div, uint32_t mod, alu_t* alu, SHORT_STDPARAMS) {
     *ret = div % mod;
 }
 
-void modulo_64(uint32_t declargs, uint64_t *ret, uint64_t div, uint64_t mod, alu_t* alu, SHORT_STDPARAMS){
+void extern_alu_modulo_64(uint64_t *ret, uint64_t div, uint64_t mod, alu_t* alu, SHORT_STDPARAMS){
     *ret = div % mod;
 }

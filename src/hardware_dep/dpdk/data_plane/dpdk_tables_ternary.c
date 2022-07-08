6802e030b36eaf8854af824e84a4acce79e922af
HAK <ge73def@gitlab.lrz.de>
2022-07-08 03:59:53 +0300

    Changes DPDK Tables Ternary push for Palmtrie

--- src/hardware_dep/dpdk/data_plane/dpdk_tables_ternary.c
+++ src/hardware_dep/dpdk/data_plane/dpdk_tables_ternary.c
@@ -4,47 +4,50 @@
 // This file is included directly from `dpdk_tables.c`.
 
 
-void ternary_create(lookup_table_t* t, int socketid)
-{
-    #ifdef T4P4S_PALMTRIE
+#ifdef T4P4S_PALMTRIE
+    void ternary_create(lookup_table_t* t, int socketid)
+    {
         struct palmtrie palmtrie;
 
         palmtrie_init(&palmtrie, PALMTRIE_BASIC);
 
         t->table = &palmtrie;
-    #else
+    }
+#else
+    void ternary_create(lookup_table_t* t, int socketid)
+    {
         t->table = naive_ternary_create(t->entry.key_size, t->max_size);
-    #endif
-}
+    }
+#endif
 
-void ternary_add(lookup_table_t* t, uint8_t* key, uint8_t* mask, uint8_t* value)
-{
-    #ifdef T4P4S_PALMTRIE
-    #else
+#ifdef T4P4S_PALMTRIE
+#else
+    void ternary_add(lookup_table_t* t, uint8_t* key, uint8_t* mask, uint8_t* value)
+    {
         if (t->entry.key_size == 0) return; // don't add lines to keyless tables
 
         uint8_t* entry = make_table_entry_on_socket(t, value);
         naive_ternary_add(t->table, key, mask, entry);
-    #endif
-}
+    }
+#endif
 
-uint8_t* ternary_lookup(lookup_table_t* t, uint8_t* key)
-{
-    #ifdef T4P4S_PALMTRIE
-    #else
+#ifdef T4P4S_PALMTRIE
+#else
+    uint8_t* ternary_lookup(lookup_table_t* t, uint8_t* key)
+    {
         if (t->entry.key_size == 0) return t->default_val;
 
         uint8_t* ret = naive_ternary_lookup(t->table, key);
         return ret == NULL ? t->default_val : ret;
-    #endif
-}
+    }
+#endif
 
-void ternary_flush(lookup_table_t* t)
-{
-    #ifdef T4P4S_PALMTRIE
-    #else
+#ifdef T4P4S_PALMTRIE
+#else
+    void ternary_flush(lookup_table_t* t)
+    {
         if (t->entry.key_size == 0) return; // nothing must have been added
 
         naive_ternary_flush(t->table);
-    #endif
-}
+    }
+#endif

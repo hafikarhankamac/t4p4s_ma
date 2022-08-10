aa6ba9a2168a5c1d7fc3b583a31c2e258f1ee668
HAK <ge73def@gitlab.lrz.de>
2022-08-10 14:30:46 +0300

    Palmtrie testing

--- src/hardware_dep/dpdk/data_plane/dpdk_lib_change_tables.c
+++ src/hardware_dep/dpdk/data_plane/dpdk_lib_change_tables.c
@@ -84,6 +84,9 @@ void lpm_add_promote(table_name_t tableid, uint8_t* key, uint8_t depth, uint8_t*
     FORALLNUMANODES("Add", "/" T4LIT(LPM), CHANGE_TABLE(lpm_add, key, depth, value), is_const_entry, should_print)
 }
 #ifdef T4P4S_PALMTRIE
+void ternary_add_promote(table_name_t tableid, uint8_t* key, uint8_t* mask, uint8_t* value, bool is_const_entry, bool should_print) {
+    FORALLNUMANODES("Add", "/" T4LIT(ternary), CHANGE_TABLE(ternary_add, key, mask, value), is_const_entry, should_print)
+}
 #else
 void ternary_add_promote(table_name_t tableid, uint8_t* key, uint8_t* mask, uint8_t* value, bool is_const_entry, bool should_print) {
     FORALLNUMANODES("Add", "/" T4LIT(ternary), CHANGE_TABLE(ternary_add, key, mask, value), is_const_entry, should_print)

fabc522891d4a40c3e040adadeff1ba721707234
HAK <ge73def@gitlab.lrz.de>
2022-08-26 00:13:36 +0300

    Ternary palmtrie implementation (lookup table final)

--- src/hardware_dep/dpdk/data_plane/dpdk_tables_ternary.c
+++ src/hardware_dep/dpdk/data_plane/dpdk_tables_ternary.c
@@ -30,7 +30,7 @@
         char edata[256];
         char emask[256];
 
-        sprintf(&strline[0], "%s %hhd.%hhd.%hhd.%hhd/%hhd", "permit ip 0.0.0.0/0", *(key+3), *(key+2), *(key+1), *(key), mask); // ACL like "permit ip 0.0.0.0/0 10.0.1.0/24"
+        sprintf(&strline[0], "%s %hhd.%hhd.%hhd.%hhd/%hhd", "permit ip 0.0.0.0/0", *(key), *(key+1), *(key+2), *(key+3), *mask); // ACL like "permit ip 0.0.0.0/0 10.0.1.0/24"
 
         RTE_LOG(INFO, USER1, "strline = %s\n", strline);
 
@@ -43,12 +43,9 @@
                            tcam_e.mask[0], tcam_e.mask[1], tcam_e.mask[2], tcam_e.mask[3], tcam_e.mask[4], tcam_e.mask[5], tcam_e.mask[6], tcam_e.mask[7],
                            tcam_e.mask[8], tcam_e.mask[9], tcam_e.mask[10], tcam_e.mask[11], tcam_e.mask[12], tcam_e.mask[13], tcam_e.mask[14], tcam_e.mask[15]);
 
-        addr_t addr_t_key;
-        addr_t addr_t_mask;
+        addr_t addr_t_key = {0, {0, 0, 0, 0, 0, 0, 0, 0}};
+        addr_t addr_t_mask = {0, {0, 0, 0, 0, 0, 0, 0, 0}};
         u64 temp;
-
-        memset(&addr_t_key, 0, sizeof(addr_t));
-        memset(&addr_t_mask, 0, sizeof(addr_t));
 
         RTE_LOG(INFO, USER1, "Before reverse edata = %s\n", edata);
         RTE_LOG(INFO, USER1, "Before reverse emask = %s\n", emask);
@@ -67,7 +64,6 @@
         }
 
         for ( int i = 0; i < 8; i++ ) {
-            //RTE_LOG(DEBUG, TABLE, "(addr_t_key[%ld]: 0x%.16lX addr_t_mask[%ld]: 0x%.16lX)\n", i, addr_t_key.a[i], i, addr_t_mask.a[i]);
             RTE_LOG(INFO, USER1, "(addr_t_key[%ld]: 0x%.16lX addr_t_mask[%ld]: 0x%.16lX)\n", i, addr_t_key.a[i], i, addr_t_mask.a[i]);
         }
 
@@ -95,7 +91,7 @@
         acl_tcam_entry_t tcam_e;
         char edata[256];
 
-        sprintf(&strline[0], "%s %hhd.%hhd.%hhd.%hhd/%hhd", "permit ip 0.0.0.0/0", *(key+3), *(key+2), *(key+1), *(key), 0); // ACL like "permit ip 0.0.0.0/0 10.0.1.0/24"
+        sprintf(&strline[0], "%s %hhd.%hhd.%hhd.%hhd/%hhd", "permit ip 0.0.0.0/0", *(key), *(key+1), *(key+2), *(key+3), 0); // ACL like "permit ip 0.0.0.0/0 10.0.1.0/24"
 
         if (parse_acl(&strline[0], &tcam_e) == (-1)) return t->default_val;
 
@@ -103,10 +99,8 @@
                            tcam_e.data[0], tcam_e.data[1], tcam_e.data[2], tcam_e.data[3], tcam_e.data[4], tcam_e.data[5], tcam_e.data[6], tcam_e.data[7],
                            tcam_e.data[8], tcam_e.data[9], tcam_e.data[10], tcam_e.data[11], tcam_e.data[12], tcam_e.data[13], tcam_e.data[14], tcam_e.data[15]);
 
-        addr_t addr_t_key;
+        addr_t addr_t_key = {0, {0, 0, 0, 0, 0, 0, 0, 0}};
         u64 temp;
-
-        memset(&addr_t_key, 0, sizeof(addr_t));
 
         palmtrie_reverse(&edata[0]);
 
@@ -115,13 +109,14 @@
             addr_t_key.a[i >> 4] |= temp << ((i & 0xf) << 2);
         }
 
-        addr_t tmp = {0, {0, 0, 0, 0, 0, 0, 0, 0}};
+        //addr_t tmp = {0, {0, 0, 0, 0, 0, 0, 0, 0}};
 
-        tmp.a[0] = 0x0200000000000000;
-        tmp.a[1] = 0x00000000000A0000;
+        //tmp.a[0] = 0x0200000000000000;
+        //tmp.a[1] = 0x00000000000A0000;
 
-        //u64 ret = palmtrie_lookup(t->table, addr_t_key);
-        u64 ret = palmtrie_lookup(t->table, tmp);
+        //u64 ret = palmtrie_lookup(t->table, tmp);
+ 
+        u64 ret = palmtrie_lookup(t->table, addr_t_key);
         return (uint8_t*)ret == NULL ? t->default_val : (uint8_t*)ret;
     }
 #else

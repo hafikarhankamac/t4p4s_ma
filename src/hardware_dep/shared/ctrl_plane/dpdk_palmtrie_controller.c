b1b4062cec7b01bce3c977e15f3333fb292f587c
HAK <ge73def@gitlab.lrz.de>
2022-09-09 03:19:09 +0300

    Experiment scaling config

--- src/hardware_dep/shared/ctrl_plane/dpdk_palmtrie_controller.c
+++ src/hardware_dep/shared/ctrl_plane/dpdk_palmtrie_controller.c
@@ -64,6 +64,20 @@ xor128(void)
     return w = (w ^ (w>>19)) ^ (t ^ (t >> 8));
 }
 
+int
+hex2bin(char c)
+{
+    if ( c >= '0' && c <= '9' ) {
+        return c - '0';
+    } else if ( c >= 'a' && c <= 'f' ) {
+        return c - 'a' + 10;
+    } else if ( c >= 'A' && c <= 'F' ) {
+        return c - 'A' + 10;
+    } else {
+        return 0;
+    }
+}
+
 controller c;
 
 void dhf(void* b) {
